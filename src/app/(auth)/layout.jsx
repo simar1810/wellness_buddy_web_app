518d67a5baf77089025d72a5364753106978650d
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2025-10-15 11:35:11 +0530

    fix: minor changes

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -5,11 +5,11 @@ export default function layout({ children }) {
     <div className="min-h-screen container flex items-center justify-between gap-10">
       <div className="max-w-[900px] w-full bg-white p-8 pt-4 mx-auto shadow-xl border-1 rounded-[20px]">
         <Image
-          src="/wz-landscape.png"
+          src="/logo.png"
           height={60}
           width={150}
           alt="Wz - logo landscape"
-          className="mx-auto mb-4"
+          className="w-[80px] h-[80px] mx-auto mb-4"
         />
         <div className="flex items-center gap-6">
           <Image
