758849132ad24541cb89e5286eb9fc1f7e342d52
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2025-12-07 22:59:38 +0530

    fix: minor changes

--- src/app/(authorized)/coach/downline/layout.jsx
+++ src/app/(authorized)/coach/downline/layout.jsx
@@ -5,7 +5,7 @@ import { useAppSelector } from "@/providers/global/hooks"
 export default function Layout({ children }) {
   const { clubType } = useAppSelector(state => state.coach.data)
 
-  if (!["System Leader", "Club Leader", "Club Leader Jr"].includes(clubType)) {
+  if (!["System Leader", "Club Leader", "Club Leader Jr", "Club Captain Jr"].includes(clubType)) {
     return <ContentError title="You are not authorized to access this page" />
   }
 
