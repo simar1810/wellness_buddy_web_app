fc08e762a99f14e4e9c25886eeab412fb462f83c
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2026-03-27 17:19:59 +0530

    fix: subscription adding membership moved from 30 days to 1 month in silver and gold

--- src/config/data/membership.js
+++ src/config/data/membership.js
@@ -1,18 +1,33 @@
+import { addDays, addMonths, format } from "date-fns";
+
 export const MEMBERSHIP_TIER_CONFIG = {
   none: {
     amount: 0,
     days: 0,
+    calcEndDate: (date) => format(date, "yyyy-MM-dd")
   },
   demo: {
     amount: 0,
     days: 2,
+    calcEndDate: (date) => format(
+      addDays(date, 2),
+      "yyyy-MM-dd"
+    )
   },
   silver: {
     amount: 4500,
     days: 30,
+    calcEndDate: (date) => format(
+      addMonths(date, 1),
+      "yyyy-MM-dd"
+    )
   },
   gold: {
     amount: 6990,
     days: 30,
+    calcEndDate: (date) => format(
+      addMonths(date, 1),
+      "yyyy-MM-dd"
+    )
   },
 }
